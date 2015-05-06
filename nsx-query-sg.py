0dfde61f7c618d893c6dd2c44b7c3e234e38f9da
dcoghlan <dale.coghlan@gmail.com>
2015-05-06 17:38:47 +1000

    Version increased to 1.0.1
    Removed trailing spaces
    When parsing xml response to string, ignore any ascii characters that have an ascii code over 128.

--- nsx-query-sg.py
+++ nsx-query-sg.py
@@ -3,8 +3,9 @@
 # Written by Dale Coghlan
 # Date: 24 Apr 2015
 # https://github.com/dcoghlan/
+# Version: 1.0.1
 
-# ------------------------------------------------------------------------------------------------------------------	
+# ------------------------------------------------------------------------------------------------------------------
 # Set some variables. No need to change anything else after this section
 
 # Set the managed object reference
@@ -18,7 +19,7 @@ _password = 'default'
 
 # Show IPv6 addressing in the output [0=No, 1=Yes].
 _showIPv6 = '1'
-# ------------------------------------------------------------------------------------------------------------------	
+# ------------------------------------------------------------------------------------------------------------------
 
 import requests
 import argparse
@@ -33,7 +34,7 @@ try:
 	import urllib3
 	requests.packages.urllib3.disable_warnings()
 except ImportError:
-	# If you don't have urllib3 we can just hide the warnings 
+	# If you don't have urllib3 we can just hide the warnings
 	logging.captureWarnings(True)
 
 parser = argparse.ArgumentParser(description='Queries NSX Manager for all members of the specified security group')
@@ -52,7 +53,7 @@ group2.add_argument('-l', help='List all security groups', dest='_listsg', actio
 args = parser.parse_args()
 
 # Check to see if the password has been hardcoded. If it hasn't prompt for the password
-try: 
+try:
 	_password
 except NameError:
 	_password = getpass.getpass(prompt='NSX Manager password:')
@@ -84,15 +85,20 @@ def f_smart_truncate(content, length, suffix='...'):
 		return content[:length-3] + suffix
 
 def f_get_sg_list():
+	print('Retrieving Security Groups via API')
+
 	# NSX API URL to get all services configured in the specified scope
 	requests_url = 'https://%s/api/2.0/services/securitygroup/scope/%s' % (_nsxmgr, _scope)
 
 	# Submits the request to the NSX Manager
 	success = requests.get((requests_url), headers=_myheaders, auth=(_user, _password), verify=False)
 
+	print('Retrieved all security group')
+
 	# Loads XML response into memory
-	doc = parseString(success.text)
-		
+	doc = parseString(success.text.encode('ascii', 'ignore'))
+
+
 	# Loads xml document into memory using the application element
 	securitygrouplist = doc.getElementsByTagName('securitygroup')
 
@@ -107,7 +113,7 @@ def f_get_sg_list():
 	for sg in securitygrouplist:
 		# clears the list
 		data = [];
-		# Gets the objectId of the application  
+		# Gets the objectId of the application
 		sgObjectId = sg.getElementsByTagName('objectId')[0]
 		#sgObjectIdData = sgObjectId[:15] + (sgObjectId[15:] and '..')
 		#print(sgObjectIdData)
@@ -162,7 +168,7 @@ def f_get_sg_mem_vms(id):
 
 	# Iterates through each application element
 	for vmnodes in _get_sec_group_member_vms_list:
-		# Gets the objectId of the member  
+		# Gets the objectId of the member
 		vmObjectId = vmnodes.getElementsByTagName('vmId')[0]
 		vmName = vmnodes.getElementsByTagName('vmName')[0]
 		print(_outputDataRow.format(vmObjectId.firstChild.data,	vmName.firstChild.data,	''))
@@ -185,7 +191,7 @@ def f_get_sg_mem_ipSets(id):
 
 	# Iterates through each application element
 	for elements in _get_sec_group_member_ipSets_list:
-		# looks for the string element.  		
+		# looks for the string element.
 		string = elements.getElementsByTagName('string')
 
 		# Checks to see if the element exists
@@ -218,7 +224,7 @@ def f_get_sg_mem_static_include(id):
 
 	# Iterates through each application element
 	for member in _sec_group_member_static_include_list:
-		# Get the objectId of the member  
+		# Get the objectId of the member
 		memberObjectId = member.getElementsByTagName('objectId')[0]
 		# Get the name of the application
 		memberObjectType = member.getElementsByTagName('objectTypeName')[0]
